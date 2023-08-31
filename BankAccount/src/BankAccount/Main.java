b61d291c009a87ed5bc59f0e384f4fdaef0b310d
somkhitp <s.phosavath@gmail.com>
2023-08-31 15:49:11 -0500

    Added more objects to the test

--- BankAccount/src/BankAccount/Main.java
+++ BankAccount/src/BankAccount/Main.java
@@ -14,13 +14,49 @@ public class Main {
 		AccountBalance accountBalance = new AccountBalance(100000);
 		
 		//Prints out the Name, Number, and Balance
+		System.out.println("************************************************");
+		System.out.println("***********| Account Holder |*******************");
 		System.out.println("Account Name: " + accountName.getName());
 		System.out.println("Account Number: " + accountNumber.getNumber());
 		System.out.println("Account Balance: " + accountBalance.getBalance());
 		
 		//Deposits and Withdrawals
-		accountBalance.deposit(50000);
+		accountBalance.deposit(20.34);
 		accountBalance.withdrawal(50);
+		
+		
+		// A Second Account
+		AccountName accountNameTwo = new AccountName("Justin McFarley");
+		AccountNumber accountNumberTwo = new AccountNumber("89234");
+		AccountBalance accountBalanceTwo = new AccountBalance(540.54);
+		
+		//Prints out the Name, Number, and Balance
+		System.out.println("************************************************");
+		System.out.println("***********| Account Holder |*******************");
+		System.out.println("Account Name: " + accountNameTwo.getName());
+		System.out.println("Account Number: " + accountNumberTwo.getNumber());
+		System.out.println("Account Balance: " + accountBalanceTwo.getBalance());
+		
+		//Deposits and Withdrawals
+		accountBalanceTwo.deposit(100.34);
+		accountBalanceTwo.withdrawal(10);
+		
+		
+		// A Third Account
+		AccountName accountNameThird = new AccountName("Russel Wong");
+		AccountNumber accountNumberThird = new AccountNumber("12714");
+		AccountBalance accountBalanceThird = new AccountBalance(3600.23);
+
+		// Prints out the Name, Number, and Balance
+		System.out.println("************************************************");
+		System.out.println("***********| Account Holder |*******************");
+		System.out.println("Account Name: " + accountNameThird.getName());
+		System.out.println("Account Number: " + accountNumberThird.getNumber());
+		System.out.println("Account Balance: " + accountBalanceThird.getBalance());
+
+		// Deposits and Withdrawals
+		accountBalanceThird.deposit(-123);
+		accountBalanceThird.withdrawal(-12.34);
 	}
 
 }
